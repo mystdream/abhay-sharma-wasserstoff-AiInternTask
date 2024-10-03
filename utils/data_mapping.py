73e6827713b6d8fb26aac2540a086a1ac679b8c2
Abhay Sharma <119748194+mystdream@users.noreply.github.com>
2024-10-03 15:25:50 +0530

    Create data_mapping.py

---
+++ utils/data_mapping.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def rle_decode(rle, shape):
+    s = rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape)
